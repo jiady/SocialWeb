36fdcfa54dcf6f71c5400812af5dd2de58d4491b
贾冬雨 <893392749@qq.com>
2015-06-02 20:07:56 +0800

    j

--- application/controllers/Testj.php
+++ application/controllers/Testj.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Test extends CI_Controller {
+class Testj extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
