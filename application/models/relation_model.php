5cde79b63d319ffebe446a7458f30c3cf404f1f6
Managine <421532821@qq.com>
2015-06-02 20:04:20 +0800

    add

---
+++ application/models/relation_model.php
@@ -0,0 +1,93 @@
+<?php
+
+class relation_model extends CI_model{
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+    function addBlackList($id, $blackid) {
+    	$query="SELECT * FROM blacklist WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($blackid);
+    	if ($this->db->query($query)->num_rows()>0)
+    		return false;
+    	$insert="INSERT INTO blacklist(from_uid, to_uid) VALUES(".$this->db->escape($id).",".$this->db->escape($blackid).")";
+    	$this->db->query($insert);
+    	if ($this->db->affected_rows>0)
+    		return true;
+    	return false;
+    }
+
+    function getBlackLists($id) {
+    	$query="SELECT to_uid FROM blacklist WHERE from_uid=".$this->db->escape($id);
+    	return $this->db->query($query)->result();
+    }
+
+    function deleteBlackList($id,$blackid) {
+    	$delete="DELETE FROM blacklist WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($blackid);
+    	$this->db->query($delete);
+    	if ($this->db->affected_rows>0)
+    		return true;
+    	return false;
+    }
+
+    function getFriends($id) {
+    	$query="SELECT to_uid FROM friend WHERE from_uid=".$this->db->escape($id);
+    	return $this->db->query($query)->result();
+    }
+
+    function deleteFriend($id, $delete_id) {
+    	$delete="DELETE FROM friend WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($delete_id);
+    	$this->db->query($delete);
+    	if ($this->db->affected_rows>0)
+    		return true;
+    	return false;
+    }
+
+    function sendFriendRequest($id, $to_id) {
+    	$query="SELECT * FROM blacklist WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($to_id);
+    	if ($this->db->query($query)->num_rows()>0)
+    		return false;
+    	$insert="INSERT INTO friendrequest(from_uid, to_uid) VALUES(".$this->db->escape($id).",".$this->db->escape($to_id).")";
+    	$this->db->query($insert);
+    	if ($this->db->affected_rows>0)
+    		return true;
+    	return false;
+    }
+
+    function sendFriendRequest($id, $to_id, $reason) {
+    	$query="SELECT * FROM blacklist WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($to_id);
+    	if ($this->db->query($query)->num_rows()>0)
+    		return false;
+    	$query="SELECT * FROM friendrequest WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($to_id)." AND reason=".$this->db->escape($reason);
+    	if ($this->db->query($query)->num_rows()>0)
+    		return false;
+        $delete="DELETE FROM friendrequest WHERE from_uid=".$this->db->escape($id)." AND to_uid=".$this->db->escape($to_id);
+        $this->db->query($delete);
+    	$insert="INSERT INTO friendrequest(from_uid, to_uid, reason) VALUES(".$this->db->escape($id).",".$this->db->escape($to_id).",".$this->db->escape($reason).")";
+    	$this->db->query($insert);
+    	if ($this->db->affected_rows>0)
+    		return true;
+    	return false;
+    }
+
+    function getFriendRequest($id) {
+    	$query="SELECT from_id,reason FROM friendrequest WHERE to_id=".$this->db->escape($id);
+    	return $this->db->query($query)->result();
+    }
+
+    function acceptFriendRequest($id, $from_id) {
+    	$update="UPDATE friendrequest SET accepted=true WHERE from_uid=".$this->db->escape($from_id)." AND to_uid=".$this->db->escape($id);
+    	$this->db->query($update);
+    	if ($this->db->affected_rows==0)
+    		return false;
+    	$insert="INSERT INTO friend(from_uid, to_uid) VALUES(".$this->db->escape($id).",".$this->db->escape($from_id).")";
+    	$this->db->query($insert);
+    	if ($this->db->affected_rows==0)
+    		return false;
+    	$insert="INSERT INTO friend(from_uid, to_uid) VALUES(".$this->db->escape($from_id).",".$this->db->escape($id).")";
+    	$this->db->query($insert);
+    	if ($this->db->affected_rows==0)
+    		return false;
+    	return true;
+    }
+}
---
+++ application/models/user_model.php
@@ -0,0 +1,46 @@
+<?php
+
+class user_model extends CI_model{
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+   
+
+    function login($email,$password){
+        if($this->session->userdata('uid'))
+            return false;//already login
+        $this->db->where('email',$email)->where('password',$password);
+        $query=$this->db->get('user');
+        if($query->num_rows()>0){
+            $row=$query->row_array();
+            unset($row['password']);
+            $this->session->sess_destroy();
+            $this->session->set_userdata($row);
+            return true;
+        }
+        return false;
+    }
+
+    function logout(){
+    	$this->session->sess_destroy();
+        return true;
+    }
+
+    function register($email,$password){
+        $this->db->where('email',$email);
+        $query=$this->db->get('user');
+        if($query->num_rows()>0){
+            return false;
+        }
+        $to_insert=array();
+        $to_insert['email']=$email;
+        $to_insert['password']=$password;
+        $this->db->insert($to_insert);
+        return $this->login($email,$password);
+    }
+    
+
+}
+
