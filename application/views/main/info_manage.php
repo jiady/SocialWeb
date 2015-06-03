72e0e764cceb71e47c35d5dbf9525fd29787ad50
Managine <421532821@qq.com>
2015-06-03 20:39:22 +0800

    x

--- application/views/main/info_manage.php
+++ application/views/main/info_manage.php
@@ -20,7 +20,7 @@ form {float:left; margin-top: 5px; margin-bottom: 5px;}
 	</form>
 	<form  method="post" action=<?=site_url('info_control/update')?> class='form-horizontal'>
 		<div class="row col-lg-10">
-			<div class="col-lg-5">
+			<div class="col-lg-10">
 				<div class="input-group">
 					<span class="input-group-addon">
 						修改<?=$part?>
@@ -28,7 +28,7 @@ form {float:left; margin-top: 5px; margin-bottom: 5px;}
 			      	<input type="text" class="form-control" name="name_old" placeholder="Old name..."/>
 		    	</div>
 		    </div>
-	    	<div class="col-lg-5">
+	    	<div class="col-lg-10">
 				<div class="input-group">
 			      	<input type="text" class="form-control" name="name_new" placeholder="New name..."/>
 			      	<span class="input-group-btn">
