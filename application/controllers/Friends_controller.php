f435e2adfb17df0d007e9cf161ed378ffef461c3
Managine <421532821@qq.com>
2015-06-03 21:06:59 +0800

    f

---
+++ application/controllers/Friends_controller.php
@@ -0,0 +1,28 @@
+<?php
+class Info_control extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->model('Relation_model');
+		$this->load->model('Userinfo_model');
+	}
+
+	function index() {
+		$this->load->view('block/header');
+		$id=$this->session->userdata("uid");
+		$info=array();
+		$res=$this->User_model->getFriends($id);
+		if (count($res)>0) {
+			foreach ($res as $row) {
+				$user_info=$this->Userinfo_model->getInfo($row->to_uid);
+				$info['url']=$this->Userinfo_model->getHeadImage($row->to_uid);
+				$info['name']=$user_info['name'];
+				$info['profile']=$user_info['profile'];
+				$this->load->view('main/friends',$info);
+			}
+		}
+		else {
+			echo "你没有好友！";
+		}
+		$this->load->view('block/footer');
+	}
