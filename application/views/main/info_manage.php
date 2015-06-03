9fb1c6f52cc3bfac2e4e3733de172d921f474305
Managine <421532821@qq.com>
2015-06-03 19:45:00 +0800

    h

--- application/views/main/info_manage.php
+++ application/views/main/info_manage.php
@@ -1,30 +1,35 @@
-<div class="container">
-	<form  method="post" action=<?=site_url('info_control/add')?>>
-		<div class="input-group">
-			<span class="input-group-addon" id="basic-addon1">
-				新增<?=$part?>
-			</span>
-	      	<input type="text" class="form-control" name="name_add" placeholder="New name..."/>
-	      	<span class="input-group-btn">
-	        	<button class="btn btn-default" type="submit">提交</button>
-     		</span>
-    	</div>
+<style type="text/css">
+div {float:left; margin-top: 10px; margin-left: 10px;}
+</style>
+
+	<form  method="post" action=<?=site_url('info_control/add')?> class='form-horizontal'>
+		<div class="col-lg-12">
+			<div class="input-group">
+				<span class="input-group-addon">
+					新增<?=$part?>
+				</span>
+		      	<input type="text" class="form-control" name="name_add" placeholder="New name..."/>
+		      	<span class="input-group-btn">
+		        	<button class="btn btn-default" type="submit">提交</button>
+	     		</span>
+	    	</div>
+	    </div>
     	<div class="input-group">
-		      	<input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
+		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-
-	<form  method="post" action=<?=site_url('info_control/update')?>>
+	<br/>
+	<form  method="post" action=<?=site_url('info_control/update')?> class='form-horizontal'>
 		<div class="row">
-			<div class="col-lg-6">
+			<div class="col-lg-12">
 				<div class="input-group">
-					<span class="input-group-addon" id="basic-addon1">
+					<span class="input-group-addon">
 						修改<?=$part?>
 					</span>
 			      	<input type="text" class="form-control" name="name_old" placeholder="Old name..."/>
 		    	</div>
 		    </div>
-		    <div class="col-lg-6">
+		    <div class="col-lg-12">
 				<div class="input-group">
 			      	<input type="text" class="form-control" name="name_new" placeholder="New name..."/>
 			      	<span class="input-group-btn">
@@ -32,24 +37,26 @@
 		     		</span>
 		    	</div>
 		    </div>
-	    <div>
+	    </div>
     	<div class="input-group">
-		      	<input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
+		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-
-	<form method="post" action=<?=site_url('info_control/delete')?>>
-		<div class="input-group">
-			<span class="input-group-addon" id="basic-addon1">
-				删除<?=$part?>
-			</span>
-	      	<input type="text" class="form-control" name="name_delete" placeholder="Name..."/>
-	      	<span class="input-group-btn">
-	        	<button class="btn btn-default" type="submit">提交</button>
-     		</span>
-    	</div>
+	<br/>
+	<form method="post" action=<?=site_url('info_control/delete')?> class='form-horizontal'>
+		<div class="col-lg-12">
+			<div class="input-group">
+				<span class="input-group-addon">
+					删除<?=$part?>
+				</span>
+		      	<input type="text" class="form-control" name="name_delete" placeholder="Name..."/>
+		      	<span class="input-group-btn">
+		        	<button class="btn btn-default" type="submit">提交</button>
+	     		</span>
+	    	</div>
+	    </div>
     	<div class="input-group">
-		      	<input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
+		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-</div>
+	<br/>
