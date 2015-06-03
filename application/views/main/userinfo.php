c511ef30302a7b602cdd49267ff33ec37b5a7eb5
Managine <421532821@qq.com>
2015-06-03 18:53:07 +0800

    s

--- application/views/main/userinfo.php
+++ application/views/main/userinfo.php
@@ -32,71 +32,89 @@
 	<div class="container">
 		<form  method="post" action=<?=site_url('userinfo_control/change')?> class='form-horizontal'>
 			<div class="row">
-				<div class="input-group">
-					<span class="input-group-addon">姓名</span>
-			      	<input type="text" class="form-control" name="name" value=<?=$info["name"]?> />
-		    	</div>
-		    	<div class="input-group">
-					<span class="input-group-addon">
-						性别
-					</span>
-					<input class="form-control typeahead" type="text" name="gender" value=<?php
-						if ($info['gender']==0)
-							echo "男";
-						else 
-							echo "女" ?> />
-		    	</div>
+				<div class="col-lg-12">
+					<div class="input-group">
+						<span class="input-group-addon">姓名</span>
+				      	<input type="text" class="form-control" name="name" value=<?=$info["name"]?> />
+			    	</div>
+			    </div>
+			    <div class="col-lg-12">
+			    	<div class="input-group">
+						<span class="input-group-addon">
+							性别
+						</span>
+						<input class="form-control typeahead" type="text" name="gender" value=<?php
+							if ($info['gender']==0)
+								echo "男";
+							else 
+								echo "女" ?> />
+			    	</div>
+			    </div>
 		    </div>
 			<br/>
-	    	<div class="input-group">
-	    		<span class="input-group-addon">
-					个性签名
-				</span>
-			    <input type="text" class="form-control" name="profile" value=<?=$info["profile"]?> />
-		    </div>
-		    <br/>
-		    <div class="input-group">
-	    		<span class="input-group-addon">
-					电子邮箱
-				</span>
-			    <input type="text" class="form-control" name="email" value=<?=$info["email"]?> />
-		    </div>
+			<div class="col-lg-12">
+		    	<div class="input-group">
+		    		<span class="input-group-addon">
+						个性签名
+					</span>
+				    <input type="text" class="form-control" name="profile" value=<?=$info["profile"]?> />
+			    </div>
+			    <br/>
+			</div>
+			<div class="col-lg-12">
+			    <div class="input-group">
+		    		<span class="input-group-addon">
+						电子邮箱
+					</span>
+				    <input type="text" class="form-control" name="email" value=<?=$info["email"]?> />
+			    </div>
+			</div>
 		    <br/>
 		    <div class="row">
-				<div class="input-group">
-					<span class="input-group-addon">
-						学历
+		    	<div class="col-lg-12">
+					<div class="input-group">
+						<span class="input-group-addon">
+							学历
+						</span>
+				      	<input type="text" class="form-control typeahead" name="edu" value=<?=$info["edu"]?> />
+			    	</div>
+			   	</div>
+			   	<div class="col-lg-12">
+			    	<div class="input-group">
+						<span class="input-group-addon">
+							入学年份
+						</span>
+						<input class="form-control" type="text" name="eduyear" value=<?=$info["eduyear"]?> />
+			    	</div>
+			    </div>
+		    </div>
+		    <br/>
+		    <div class="col-lg-12">
+			    <div class="input-group">
+		    		<span class="input-group-addon">
+						所在学校
 					</span>
-			      	<input type="text" class="form-control typeahead" name="edu" value=<?=$info["edu"]?> />
-		    	</div>
-		    	<div class="input-group">
-					<span class="input-group-addon">
-						入学年份
+				    <input id="school_enter" type="text" class="form-control typeahead" name="school" data-provide="typeahead" value=<?=$info["school"]?> />
+			    </div>
+			</div>
+		    <br/>
+		    <div>
+			    <div class="input-group">
+		    		<span class="input-group-addon">
+						所学专业
 					</span>
-					<input class="form-control" type="text" name="eduyear" value=<?=$info["eduyear"]?> />
-		    	</div>
-		    </div>
+				    <input id="major_enter" type="text" class="form-control typeahead" name="major" data-provide="typeahead" value=<?=$info["major"]?> />
+			    </div>
+			</div>
 		    <br/>
-		    <div class="input-group">
-	    		<span class="input-group-addon">
-					所在学校
-				</span>
-			    <input id="school_enter" type="text" class="form-control typeahead" name="school" data-provide="typeahead" value=<?=$info["school"]?> />
-		    </div>
-		    <br/>
-		    <div class="input-group">
-	    		<span class="input-group-addon">
-					所学专业
-				</span>
-			    <input id="major_enter" type="text" class="form-control typeahead" name="major" data-provide="typeahead" value=<?=$info["major"]?> />
-		    </div>
-		    <br/>
-		    <div class="input-group">
-	    		<span class="input-group-addon">
-					所在城市
-				</span>
-			    <input id="city_enter" type="text" class="form-control typeahead" name="city" data-provide="typeahead" value=<?=$info["city"]?> />
-		    </div>
+		    <div>
+			    <div class="input-group">
+		    		<span class="input-group-addon">
+						所在城市
+					</span>
+				    <input id="city_enter" type="text" class="form-control typeahead" name="city" data-provide="typeahead" value=<?=$info["city"]?> />
+			    </div>
+			</div>
 		    <br/>
 		    <div>
 		    	<button type="submit" class="btn btn-default">提交</button>
