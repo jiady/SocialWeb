c8c1aad335a3ed26e2e0826888d49de23decd96f
Managine <421532821@qq.com>
2015-06-03 20:04:12 +0800

    g

--- application/views/main/info_manage.php
+++ application/views/main/info_manage.php
@@ -3,7 +3,7 @@ div {float:left; margin-top: 5px; margin-left: 5px;}
 </style>
 
 	<form  method="post" action=<?=site_url('info_control/add')?> class='form-horizontal'>
-		<div class="col-lg-12">
+		<div class="col-lg-6">
 			<div class="input-group">
 				<span class="input-group-addon">
 					新增<?=$part?>
@@ -18,29 +18,31 @@ div {float:left; margin-top: 5px; margin-left: 5px;}
 		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-	<br/>
 	<form  method="post" action=<?=site_url('info_control/update')?> class='form-horizontal'>
-		<div class="row col-lg-12">
-			<div class="input-group">
-				<span class="input-group-addon">
-					修改<?=$part?>
-				</span>
-		      	<input type="text" class="form-control" name="name_old" placeholder="Old name..."/>
-	    	</div>
-			<div class="input-group">
-		      	<input type="text" class="form-control" name="name_new" placeholder="New name..."/>
-		      	<span class="input-group-btn">
-		        	<button class="btn btn-default" type="submit">提交</button>
-	     		</span>
-	    	</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="input-group">
+					<span class="input-group-addon">
+						修改<?=$part?>
+					</span>
+			      	<input type="text" class="form-control" name="name_old" placeholder="Old name..."/>
+		    	</div>
+		    </div>
+	    	<div class="col-lg-6">
+				<div class="input-group">
+			      	<input type="text" class="form-control" name="name_new" placeholder="New name..."/>
+			      	<span class="input-group-btn">
+			        	<button class="btn btn-default" type="submit">提交</button>
+		     		</span>
+		    	</div>
+		    </div>
 	    </div>
     	<div class="input-group">
 		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-	<br/>
 	<form method="post" action=<?=site_url('info_control/delete')?> class='form-horizontal'>
-		<div class="col-lg-12">
+		<div class="col-lg-6">
 			<div class="input-group">
 				<span class="input-group-addon">
 					删除<?=$part?>
@@ -55,4 +57,3 @@ div {float:left; margin-top: 5px; margin-left: 5px;}
 		    <input type="hidden" class="form-control" name="part" value=<?=$part?> placeholder="Old name..."/>
 	    </div>
 	</form>
-	<br/>
