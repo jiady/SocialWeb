7090127523bb3db28024411a789d96d221c84fe8
贾冬雨 <893392749@qq.com>
2015-06-06 17:44:23 +0800

    s

--- application/controllers/Feed.php
+++ application/controllers/Feed.php
@@ -49,6 +49,7 @@ class Feed extends CI_Controller {
     		 ->set_content_type('application/json');
     	$feed=$this->input->post('feed_content');
     	$pictures=$this->input->post('pictures');
+    	var_dump($pictures);
     	$pictures=json_decode($pictures);
     	var_dump($pictures);
     	$postfeed['content']=$feed;
