5cc1b0fb59a32cb8b2c74cd3e645363e1f6bc997
Managine <421532821@qq.com>
2015-06-06 23:50:01 +0800

    v

--- application/views/main/requests.php
+++ application/views/main/requests.php
@@ -3,15 +3,6 @@ div {margin-top: 10px;}
 #request.col-lg-10 {border-style: solid; border-width: 1px; border-color:black;background-color: rgb(221,254,255);}
 </style>
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js">
-$("#acc_but").click(function() {
-		console.log("function called");
-		$.post(<?="\"".site_url('friends_control/accept')."\""?>, {"from_uid": <?="\"".$from_uid."\"" ?>}, function() {
-			window.location.assign(<?="\"".site_url('friends_control/accept')."\"" ?>);
-		});
-	});
-</script>
-
 <div  id="request" class="col-lg-10">
 	<div class="media col-lg-10">
 	    <div class="media-left">
@@ -28,9 +19,19 @@ $("#acc_but").click(function() {
 	        	if ($accepted)
 	        		echo "<p>已接受</p> ";
 	        	else {
-	        		echo "<button id='acc_but' class='btn btn-default' type='button'>接受</button> ";
+	        		echo "<button id=\"acc_but\" class='btn btn-default'>接受</button> ";
 	        	}
 	        ?>
 	    </div>
 	</div>
 </div>
+
+<scrip src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript" >
+$("#acc_but").click(function() {
+		console.log("function called");
+		$.post(<?="\"".site_url('friends_control/accept')."\""?>, {"from_uid": <?="\"".$from_uid."\"" ?>}, function() {
+			window.location.assign(<?="\"".site_url('friends_control/accept')."\"" ?>);
+		});
+	});
+</script>
