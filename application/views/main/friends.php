7669949ca7481f3074bfa1e9c79101ad40d6af61
Managine <421532821@qq.com>
2015-06-06 19:45:07 +0800

    dz

--- application/views/main/friends.php
+++ application/views/main/friends.php
@@ -3,14 +3,16 @@ div {margin-top: 10px;}
 .media {border-style: solid; border-width: 1px; border-color:black;background-color: rgb(252,255,200);}
 </style>
 
-<div class="media">
-    <div class="media-left">
-        <a href="#">
-          <img class="media-object" src=<?=$url?> alt="HeadImage">
-        </a>
-    </div>
-    <div class="media-body">
-        <h4 class="media-heading"> <?=$name?> </h4>
-        <p> <?=$profile?></p>
-    </div>
+<div class="col-lg-10">
+	<div class="media">
+	    <div class="media-left">
+	        <a href="#">
+	          <img class="media-object" src=<?=$url?> alt="HeadImage">
+	        </a>
+	    </div>
+	    <div class="media-body">
+	        <h4 class="media-heading"> <?=$name?> </h4>
+	        <p> <?=$profile?></p>
+	    </div>
+	</div>
 </div>
--- application/views/main/requests.php
+++ application/views/main/requests.php
@@ -3,29 +3,33 @@ div {margin-top: 10px;}
 .media {border-style: solid; border-width: 1px; border-color:black;background-color: rgb(221,254,255);}
 </style>
 
-<div class="media">
-    <div class="media-left">
-        <a href="#">
-          <img class="media-object" src=<?=$url?> alt="HeadImage">
-        </a>
-    </div>
-    <div class="media-body">
-        <h4 class="media-heading"> <?=$name?> </h4>
-        <p> <?=$reason?></p>
-    </div>
-    <div class="media-right">
-        <?php 
-        	if ($accepted)
-        		echo "<p>已接受</p> ";
-        	else {
-        		echo "<button id='acc_button' class='btn btn-default' type='button'>接受</button> ";
-        	}
-        ?>
-    </div>
+<div class="col-lg-10">
+	<div class="media">
+	    <div class="media-left">
+	        <a href="#">
+	          <img class="media-object" src=<?=$url?> alt="HeadImage">
+	        </a>
+	    </div>
+	    <div class="media-body">
+	        <h4 class="media-heading"> <?=$name?> </h4>
+	        <p> <?=$reason?></p>
+	    </div>
+	    <div class="media-right">
+	        <?php 
+	        	if ($accepted)
+	        		echo "<p>已接受</p> ";
+	        	else {
+	        		echo "<button id='acc_button' class='btn btn-default' type='button'>接受</button> ";
+	        	}
+	        ?>
+	    </div>
+	</div>
 </div>
 
-<script type="text/javascript" src="jquery.js">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js">
 $("#acc_button").click(function() {
-		$.post(<?=site_url('friend_control/accept')?>, {"from_uid": <?=$from_uid?>});
+		$.post(<?=site_url('friend_control/accept')?>, {"from_uid": <?=$from_uid?>}, function() {
+			window.location.assign(<?=site_url('friend_control')?>);
+		});
 	});
 </script>
