01696ac480f2b4eb2259e9d6e2bcdede7ecb9ab1
Managine <421532821@qq.com>
2015-06-07 16:08:59 +0800

    v

--- application/views/main/tag_manage.php
+++ application/views/main/tag_manage.php
@@ -2,15 +2,15 @@
 div.tags {margin-top: 5px; padding:1px;border-style: solid; border-width: 1px; border-color:black;}
 </style>
 
-<div class="tags row col-lg-6 colorful">
-	<div class="col-lg-4">
+<div class="tags row col-lg-8 colorful">
+	<div class="col-lg-6">
 		<p><?=$content?></p>
 	</div>
 	<div class="col-lg-2">
 		<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?> > <?
 			if ($status==0)
 				echo "添加";
-			else if ($status==1)
+			else ($status==1)
 				echo "移除";
 		?></button>
 	</div>
--- application/views/main/userinfo_script.php
+++ application/views/main/userinfo_script.php
@@ -1,5 +1,5 @@
 <script>
-$("div.colorful").each(function() {
+$("div.colorful").each(function(index, element) {
 	var r=getRandomNum(0, 255);
 	var g=getRandomNum(0, 255);
 	var b=getRandomNum(0, 255);
