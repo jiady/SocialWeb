ed9c64be62421c5d625bc8344a76afe521e61520
Managine <421532821@qq.com>
2015-06-07 10:49:52 +0800

    g

--- application/views/block/footer.php
+++ application/views/block/footer.php
@@ -30,6 +30,31 @@ $("#moveBlack_button").click(function() {
       window.location.assign(<?="\"".site_url('friends_control')."\"" ?>);
     });
   });
+$("#search_button").click(function() {
+    var content=eval(document.getElementById("search_content")).value;
+    var reason=eval(document.getElementById("search_reason")).value;
+    $.post(<?="\"".site_url('friends_control/search')."\""?>, {"search_content": content, "reason": reason}, function(data) {
+        if (data==0) {
+            $("#status_tag").hide();
+        }
+        else {
+            $("#status_tag").show();
+            var tag=document.getElementById('status_tag');
+            if (data==1) {
+                tag.setAttribute("class","alert alert-success");
+                tag.innerHTML="成功发送了好友请求！";
+            }
+            else if (data==2) {
+                tag.setAttribute("class","alert alert-warning");
+                tag.innerHTML="存在重名！请用ID查询！";
+            }
+            else if (data==3) {
+                tag.setAttribute("class","alert alert-danger");
+                tag.innerHTML="错误！用户不存在！";
+            }
+        }
+    });
+  });
 </script>
 <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 <script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
--- application/views/main/search.php
+++ application/views/main/search.php
@@ -1,10 +1,10 @@
-	<form  method="post" action=<?=site_url('friends_control/search')?> class='form-horizontal'>
+	<form  method="post" class='form-horizontal'>
 		<div class="col-lg-10">
 			<div class="input-group">
 				<span class="input-group-addon">
 					查找用户：
 				</span>
-		      	<input type="text" class="form-control" name="search_content" placeholder="ID或姓名"/>
+		      	<input type="text" class="form-control" id="search_content" placeholder="ID或姓名"/>
 	    	</div>
 	    </div>
 	    <div class="col-lg-10">
@@ -12,16 +12,16 @@
 				<span class="input-group-addon">
 					申请理由：
 				</span>
-		      	<input type="text" class="form-control" name="search_reason" value="Hello" placeholder="申请理由"/>
+		      	<input type="text" class="form-control" id="search_reason" value="Hello" placeholder="申请理由"/>
 		      	<span class="input-group-btn">
-		        	<button class="btn btn-default" type="submit">提交</button>
+		        	<button class="btn btn-default" id="search_button" type="button">搜索并发送请求</button>
 	     		</span>
 	    	</div>
 	    </div>
 	</form>
 
 	<div class="col-lg-10">
-		<div role="alert" style="display:none;" class="alert alert-success">
+		<div id="status_tag" role="alert" style="display:none;" class="alert alert-success">
 			成功发送了好友申请
 		</div>
 	</div>
