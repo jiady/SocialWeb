dd309f036d6941b4fddeccab250874bff7bb071f
Managine <421532821@qq.com>
2015-06-07 16:57:43 +0800

    c

--- application/views/main/tag_manage.php
+++ application/views/main/tag_manage.php
@@ -2,12 +2,15 @@
 div.tags {margin-top: 1px;}
 </style>
 
-<div class="col-lg-10">
+<div class="row col-lg-10">
+	<div class="col-lg-2">
+		<p> </p>
+	</div>
 	<div class="tags row col-lg-6 pull-center colorful">
-		<div class="col-lg-5">
+		<div class="col-lg-4">
 			<p><?=$content?></p>
 		</div>
-		<div class="col-lg-1 pull-right">
+		<div class="col-lg-2 pull-right">
 			<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?>> 
 			<?php
 				if ($status=="0")
@@ -16,5 +19,8 @@ div.tags {margin-top: 1px;}
 					echo "移除";
 			?></button>
 		</div>
+	</div>
+	<div class="col-lg-2">
+		<p> </p>
 	</div>
 </div>
