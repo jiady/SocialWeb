c4f42df2e13e60f3687b3d7472bd70ea6989c1f5
Managine <421532821@qq.com>
2015-06-07 16:55:32 +0800

    b

--- application/views/main/tag_manage.php
+++ application/views/main/tag_manage.php
@@ -1,18 +1,20 @@
 <style type="text/css">
-div.tags {margin-top: 5px; padding:1px; border-color:black;}
+div.tags {margin-top: 1px;}
 </style>
 
-<div class="tags row col-lg-8 colorful">
-	<div class="col-lg-6">
-		<p><?=$content?></p>
-	</div>
-	<div class="col-lg-2 pull-right">
-		<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?>> 
-		<?php
-			if ($status=="0")
-				echo "添加";
-			else
-				echo "移除";
-		?></button>
+<div class="col-lg-10">
+	<div class="tags row col-lg-6 pull-center colorful">
+		<div class="col-lg-5">
+			<p><?=$content?></p>
+		</div>
+		<div class="col-lg-1 pull-right">
+			<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?>> 
+			<?php
+				if ($status=="0")
+					echo "添加";
+				else
+					echo "移除";
+			?></button>
+		</div>
 	</div>
 </div>
