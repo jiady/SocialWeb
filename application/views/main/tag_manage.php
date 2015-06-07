0f21d39d86838b929cb761fe78925bce7372108e
Managine <421532821@qq.com>
2015-06-07 16:10:32 +0800

    b

--- application/views/main/tag_manage.php
+++ application/views/main/tag_manage.php
@@ -7,10 +7,11 @@ div.tags {margin-top: 5px; padding:1px;border-style: solid; border-width: 1px; b
 		<p><?=$content?></p>
 	</div>
 	<div class="col-lg-2">
-		<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?> > <?
-			if ($status==0)
+		<button type="button button-success changeTag_button" content=<?=$content?> status=<?=$status?>> 
+		<?
+			if ($status=="0")
 				echo "添加";
-			else ($status==1)
+			else ($status=="1")
 				echo "移除";
 		?></button>
 	</div>
