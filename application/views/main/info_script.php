7935a4ae50de2b2145de87ff82c2a9a2f301087f
Managine <421532821@qq.com>
2015-06-09 14:39:47 +0800

    m

--- application/views/main/info_script.php
+++ application/views/main/info_script.php
@@ -5,7 +5,7 @@ $(".changeTag_button").click(function() {
     $.post(<?="\"".site_url('info_control/deleteTag')."\""?>, {"tag": $(this).attr("content"), "status": $(this).attr("status")}, function(data) {
     		console.log("succeed");
 });
-$(".submit_tag").click(function() {
+$("#submit_tag").click(function() {
 	var content=$("#tag_name_input").val();
 	console.log(content);
 	var to_add='<div class="row col-lg-10" id='+content+' ><div class="col-lg-2"><p> </p></div><div class="tags row col-lg-6 pull-center colorful well"><div class="col-lg-3"><p>'+content+'</p></div><div class="col-lg-3 pull-right"><button class=\'btn btn-warning changeTag_button\' content='+content+' status="1" >移除</button></div></div><div class="col-lg-2"><p> </p></div></div>';
