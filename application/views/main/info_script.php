fa58c001fe57d9fc3a31cd1c1b0d00aa6add849c
Managine <421532821@qq.com>
2015-06-09 15:08:09 +0800

    t

--- application/views/main/info_script.php
+++ application/views/main/info_script.php
@@ -8,23 +8,22 @@ $(".changeTag_button").click(function() {
 $("#submit_tag").click(function() {
 	var content=$("#tag_name_input").attr("value");
 	console.log(content);
-	var to_add=
-		'<div class="row col-lg-10" id='+content+' >'+
-			'<div class="col-lg-2">'+
-			'	<p> </p>'+
-			'</div>'+
-			'<div class="tags row col-lg-6 pull-center colorful well">'+
-				'<div class="col-lg-3">'+
-					'<p>'+content+'</p>'+
-				'</div>'+
-				'<div class="col-lg-3 pull-right">'+
-					'<button class="btn btn-warning changeTag_button" content='+content+' status="1" >移除</button>'+
-				'</div>'+
-			'</div>'+
-			'<div class="col-lg-2">'+
-				'<p> </p>'+
-			'</div>'+
-		'</div>';
+	var to_add='<div class="row col-lg-10" id='+content+' >'
+	to_add+='<div class="col-lg-2">'
+	to_add+='<p> </p>'
+	to_add+='</div>'
+	to_add+='<div class="tags row col-lg-6 pull-center colorful well">'
+	to_add+='<div class="col-lg-3">'
+	to_add+='<p>'+content+'</p>'
+	to_add+='</div>'
+	to_add+='<div class="col-lg-3 pull-right">'
+	to_add+='<button class="btn btn-warning changeTag_button" content='+content+' status="1" >移除</button>'
+	to_add+='</div>'
+	to_add+='</div>'
+	to_add+='<div class="col-lg-2">'
+	to_add+='<p> </p>'
+	to_add+='</div>'
+	to_add+='</div>';
     $("#rag_management").after(to_add);
     $.post(<?="\"".site_url('userinfo_control/addTag')."\""?>, {"tag": $(this).attr("content")}, function(data) {
             console.log("succeed");
