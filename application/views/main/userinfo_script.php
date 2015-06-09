2e8ce1145b9b6f607408098cdd88a863582e67fc
Managine <421532821@qq.com>
2015-06-09 13:59:21 +0800

    v

--- application/views/main/userinfo_script.php
+++ application/views/main/userinfo_script.php
@@ -2,6 +2,7 @@
 var to_delete;
 var to_add;
 var add=0;
+var added;
 $("div.colorful").each(function(index, element) {
 	var r=Math.round(Math.random()*96)+160;
 	var g=Math.round(Math.random()*96)+160;
@@ -16,13 +17,15 @@ $(".changeTag_button").click(function() {
     to_add='<div class="row col-lg-10" id='+$(this).attr("content")+'>'+to_delete.html()+'</div>';
     if (add=="0") {
         $("#current-not-tag").before(to_add);
-        to_add.find(".changeTag_button").html("移除");
-        to_add.find(".changeTag_button").attr("status","1");
+        added=$("#current-not-tag").prev();
+        added.find(".changeTag_button").html("移除");
+        added.find(".changeTag_button").attr("status","1");
     }
     else {
         $("#current-not-tag").after(to_add);
-        to_add.find(".changeTag_button").html("添加");
-        to_add.find(".changeTag_button").attr("status","0");
+        added=$("#current-not-tag").next();
+        added.find(".changeTag_button").html("添加");
+        added.find(".changeTag_button").attr("status","0");
     }
     to_delete.remove();
     $.post(<?="\"".site_url('userinfo_control/changeTag')."\""?>, {"tag": $(this).attr("content"), "status": $(this).attr("status")}, function(data) {
