0d8cec7b39ab2438d3b680c70d18314b9e10ca65
Managine <421532821@qq.com>
2015-06-09 15:33:17 +0800

    y

--- application/views/main/info_script.php
+++ application/views/main/info_script.php
@@ -25,7 +25,7 @@ $("#submit_tag").click(function() {
 	to_add+='</div>';
 	to_add+='</div>';
     $("#rag_management").after(to_add);
-    $.post(<?="\"".site_url('userinfo_control/addTag')."\""?>, {"tag": $(this).attr("content")}, function(data) {
+    $.post(<?="\"".site_url('info_control/addTag')."\""?>, {"tag": $(this).attr("content")}, function(data) {
             console.log("succeed");
         });
 });
