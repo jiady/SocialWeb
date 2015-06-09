34aedf0a75ceda9bfd2d6110667a5f7aacf9da60
Managine <421532821@qq.com>
2015-06-09 14:47:26 +0800

    t

--- application/views/main/info_script.php
+++ application/views/main/info_script.php
@@ -6,9 +6,25 @@ $(".changeTag_button").click(function() {
     		console.log("succeed");
 });
 $("#submit_tag").click(function() {
-	var content=$("#tag_name_input").val();
+	var content=$("#tag_name_input").attr("value");
 	console.log(content);
-	var to_add='<div class="row col-lg-10" id='+content+' ><div class="col-lg-2"><p> </p></div><div class="tags row col-lg-6 pull-center colorful well"><div class="col-lg-3"><p>'+content+'</p></div><div class="col-lg-3 pull-right"><button class=\'btn btn-warning changeTag_button\' content='+content+' status="1" >移除</button></div></div><div class="col-lg-2"><p> </p></div></div>';
+	var to_add='
+		<div class="row col-lg-10" id='+content+' >
+			<div class="col-lg-2">
+				<p> </p>
+			</div>
+			<div class="tags row col-lg-6 pull-center colorful well">
+				<div class="col-lg-3">
+					<p>'+content+'</p>
+				</div>
+				<div class="col-lg-3 pull-right">
+					<button class="btn btn-warning changeTag_button" content='+content+' status="1" >移除</button>
+				</div>
+			</div>
+			<div class="col-lg-2">
+				<p> </p>
+			</div>
+		</div>';
     $("#rag_management").after(to_add);
     $.post(<?="\"".site_url('userinfo_control/addTag')."\""?>, {"tag": $(this).attr("content")}, function(data) {
             console.log("succeed");
