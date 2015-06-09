f6a45c3b27b84e75136a6a349d42be52309e057b
Managine <421532821@qq.com>
2015-06-09 13:50:34 +0800

    d

--- application/views/main/userinfo_script.php
+++ application/views/main/userinfo_script.php
@@ -15,10 +15,16 @@ $(".changeTag_button").click(function() {
     console.log(to_delete);
     to_delete.hide();
     to_add='<div class="row col-lg-10" id='+$(this).attr("content")+'>'+to_delete.html()+'</div>';
-    if (add=="0")
+    if (add=="0") {
         $("#current-tag").after(to_add);
-    else
+        to_add.find(".changeTag_button").innerHTML("移除");
+        to_add.find(".changeTag_button").attr("status","1");
+    }
+    else {
         $("#current-not-tag").after(to_add);
+        to_add.find(".changeTag_button").innerHTML("添加");
+        to_add.find(".changeTag_button").attr("status","0");
+    }
     to_delete.remove();
     $.post(<?="\"".site_url('userinfo_control/changeTag')."\""?>, {"tag": $(this).attr("content"), "status": $(this).attr("status")}, function(data) {
             console.log("succeed");
