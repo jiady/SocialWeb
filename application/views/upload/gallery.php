19ae77693a5d4c262758bbc64240bd86526a898e
贾冬雨 <893392749@qq.com>
2015-07-01 16:04:22 +0800

    s

--- application/views/upload/gallery.php
+++ application/views/upload/gallery.php
@@ -51,7 +51,7 @@
 
 <?php $count=0; ?>
 
-<?php foreach($gallery as $info): $gid=$info->gid; $url=$info->url ?>
+<?php foreach($gallery as $info): $gid=$info->gid; $url=$info->url; $seq=$info->seq; ?>
 <?php if($count%3==0) echo "<div class='row' >"; $count++ ?>
 <div class=" col-md-4 myclass animated"  id=<?="image".$gid?>>
     <div class="thumbnail ">
@@ -59,8 +59,13 @@
       <div class="caption">
 
         <p class="text-right">
-        <button  class="btn btn-success set_head" role="button" gid=<?=$gid?> >设为头像</button>
-        <button class="btn btn-danger delete_button"  role="button" gid=<?=$gid?> >删除</button>
+
+        <?php var_dump($seq) ;if($seq==0):?>
+            <button  class="btn btn-default disable" role="button" gid=<?=$gid?> >已经设为头像，不可删除</button>
+        <?php else:?>
+            <button  class="btn btn-success set_head" role="button" gid=<?=$gid?> >设为头像</button>
+            <button class="btn btn-danger delete_button"  role="button" gid=<?=$gid?> >删除</button>
+        <?php endif?>
         </p>
 
       </div>
