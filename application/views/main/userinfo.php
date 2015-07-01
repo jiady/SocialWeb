ead1ace784176566a308c09701186cd52bfe3444
Managine <421532821@qq.com>
2015-07-01 14:51:07 +0800

    t

--- application/views/main/userinfo.php
+++ application/views/main/userinfo.php
@@ -21,15 +21,15 @@ div.line {margin-top: 10px;}
 			    </div>
 			     
 			    <div class="col-lg-5">
-			    	<div class="panel">
-						<span class="pull-left">性别</span>
+			    	<div>
+						<span class="input-group-addon">性别</span>
 						<span class="col-lg-2">
 							<input type="radio" id="gendM" name="genderM" <?php
 								if ($info['gender']==0)
 									echo "checked"?> />
 							<label>男</label>
 						</span>
-						<span class="col-lg-2 pull-right">
+						<span class="col-lg-2">
 							<input type="radio" id="gendF" name="genderF" <?php
 								if ($info['gender']==1)
 									echo "checked"?> />
